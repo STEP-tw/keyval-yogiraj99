b2445d848d8a6721e60b53a8d65643861c950745
yogiraj_tambake <ytambake@gmail.com>
2018-01-08 18:17:37 +0530

    removed skipped test from extra_test file because it is already in another testFile

--- test/testCaseSensitiveStrictParser_Extra.js
+++ test/testCaseSensitiveStrictParser_Extra.js
@@ -65,13 +65,3 @@ describe("strict parser that is case insensitive extra",function(){
     assert.deepEqual(parsed,expected);
   });
 });
-
-describe.skip("strict parser that is case sensitive",function(){
-  it("should throw error when specified keys are in lower case and actual is not",function(){
-    let kvParser=new StrictParser(["name"],true);
-    // true indicates that parser is case sensitive
-    assert.throws(()=>{
-      kvParser.parse("NAME=jayanth");
-    })
-  });
-});
