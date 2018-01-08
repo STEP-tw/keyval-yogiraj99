7503428f43e8ca630739bc8e9ece13b463f2a793
Srijayanth Sridhar <srijayanth@gmail.com>
2018-01-08 14:58:32 +0530

    added case sensitive failing test

---
+++ test/testCaseSensitiveStrictParser.js
@@ -0,0 +1,16 @@
+const src=function(filePath){return "../src/"+filePath};
+
+const assert=require('chai').assert;
+const Parsed=require(src('parsed.js'));
+const StrictParser=require(src('index.js')).StrictParser;
+
+describe("strict parser that is case insensitive",function(){
+  it("should parse when specified keys are in lower case and actual is not",function(){
+    let kvParser=new StrictParser(["name"],false);
+    // false indicates that case sensitive is false. By default it is true
+    let expected=new Parsed();
+    expected["NAME"]="jayanth";
+    let parsed=kvParser.parse("NAME=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+});
